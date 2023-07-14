bea845e2b700e74cf24db07694fed2dc1f09dc87
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-14 16:53:17 -0700

    mad combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1; num2 <= 99; num++)
+		{
+			if (num2 != num1)
+			{
+				putchar('0' + (num1 / 10));
+				putchar('0' + (num1 % 10));
+				putchar(' ');
+				putchar('0' + (num2 / 10));
+				putchar('0' + (num2 % 10));
+				putchar(',');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

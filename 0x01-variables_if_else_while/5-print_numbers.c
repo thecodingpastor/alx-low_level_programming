8efd0999297f2cc0d3269554c293521899a4698f
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-14 13:40:18 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int a = 0; a < 10;  a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}

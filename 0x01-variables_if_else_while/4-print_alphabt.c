ea04b1b4c52ad1d6cb10501fa426fa7b62049e24
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-14 13:26:52 -0700

    alphabet minus some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+		{
+			continue;
+		} else
+		{
+			putchar(letter);
+		}
+	}
+
+	return (0);
+}

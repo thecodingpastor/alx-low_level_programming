5c156f2340cd1317033dc157ac391a953ad68ec2
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-18 12:52:02 -0700

    alphabet with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Print alphabets using _putchar
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}

1a51b0fdbb69346bde4aba702e6f57af7333fc3e
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-19 13:39:53 -0700

    add multiples of 3 or 5 below 1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,23 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Adds the multiples of 3 or 5 below 1024
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 3; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+}

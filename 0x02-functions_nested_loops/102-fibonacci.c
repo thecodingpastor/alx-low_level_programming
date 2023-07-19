beb7bce3409c18d3c974fe1be2e132bb43347754
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-19 16:19:57 -0700

    up

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints first 98 fibonacci sequence
+ * main - fibonacci to 50
  *
  * Return: Always 0 (Success)
  */
@@ -17,13 +17,4 @@ int main(void)
 
 	printf("%lu, %lu, %lu, ", t1, t2, nextTerm);
 
-	for (i = 5; i <= 98; i++)
-	{
-		t1 = t2;
-		t2 = nextTerm;
-		nextTerm = t1 + t2;
-		printf("%lu, ", nextTerm);
-	}
-	printf("\n");
-	return (0);
-}
+	for 

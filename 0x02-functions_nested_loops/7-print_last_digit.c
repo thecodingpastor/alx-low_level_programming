4a097dd0e7a9974288eb7a6bfa38e11ed014f5db
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-18 17:50:03 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,13 @@
 int print_last_digit(int n)
 {
 	int num;
-	
+
 	if (n < 0)
 	{
 		n = -n;
 	}
 	num = n % 10;
+
 	if (num < 0)
 	{
 		num = -num;
@@ -21,4 +22,3 @@ int print_last_digit(int n)
 	_putchar(num + '0');
 	return (num);
 }
-

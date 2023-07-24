4d2bd3884aca623ef87f7ee727499c62c6488ae7
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-24 14:44:11 -0700

    puts skip

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * starting with the first character,
+ * followed by a new line
+ * @s: string to be processed
+ */
+
+void puts2(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s += 2;
+	}
+	_putchar('\n');
+}

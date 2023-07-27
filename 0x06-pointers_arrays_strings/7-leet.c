85d9df13294df4f3b94493dfb5aaa86cace68510
Ayeni Michael <thecodingpastor@gmail.com>
2023-07-27 16:18:42 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337.
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
+
+char *leet(char *n)
+{
+	int i, j;
+	char str1[] = "aAeEoOtTlL";
+	char str2[] = "4433007711";
+	
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == str1[j])
+			{
+				n[i] = str2[j];
+			}
+		}
+	}
+	return (n);
+}

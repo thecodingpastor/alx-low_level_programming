4a5910a1a3dd19315eddf5ebf574596485e4cb7d
Ayeni Michael <thecodingpastor@gmail.com>
2023-08-04 15:46:23 -0700

    my name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of args passed
+ * @argc: number of args passed into main
+ * @argv: array of string args
+ *
+ * Return: a number
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc);
+}

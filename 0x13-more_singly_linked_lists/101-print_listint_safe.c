2810d55ad8d1b59165e846bf97e187ffe53f4afb
Ayeni Michael <thecodingpastor@gmail.com>
2023-08-30 02:15:46 -0700

    free list safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,6 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: head of linked list
+ * Return: 
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list.
+ * @h: head to be freed
+ * Return: size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t i = 0;
+	int difference;
+	listint_t *tmp;
+
+	if (!h || !*h)
+		return (0);
+
+	for (i = 0; *h != NULL; i++)
+	{
+		difference = *h - (*h)->next;
+		if (difference > 0)
+		{
+			tmp = (*h)->next;
+			free(*h), *h = tmp;
+		}
+		else
+		{
+			free(*h), *h = NULL, break;
+		}
+	}
+	*h = NULL;
+
+	return (i);
+}

352a75be835ebb1ab22afa85ed98fd4c99cde990
Ayeni Michael <thecodingpastor@gmail.com>
2023-09-05 00:29:14 -0700

    edited descriptions in append text file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,6 +3,8 @@
 /**
  * append_text_to_file - Appends text to file.
  * @filename: A pointer to the name of the file.
+ * @text_content: arg 2
+ * Return:  1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {

a5204edc2b1780b15880d66a22b96367a427b38f
Ayeni Michael <thecodingpastor@gmail.com>
2023-09-04 21:09:43 -0700

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text to file.
+ * @filename: A pointer to the name of the file.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int openFile, writeFile, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	openFile = open(filename, O_WRONLY | O_APPEND);
+	writeFile = write(openFile, text_content, len);
+
+	if (openFile == -1 || writeFile == -1)
+		return (-1);
+
+	close(openFile);
+
+	return (1);
+}

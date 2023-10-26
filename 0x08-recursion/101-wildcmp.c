1cf866f46010d2987e6ded7b187e683c821ddd07
Ayeni Michael <thecodingpastor@gmail.com>
2023-10-26 15:54:15 +0000

    wild compare

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
-int wildcmp()
+/**
+ *wildcmp - wild comparison
+ *@one: arg 1
+ *@two: arg 2
+ *Return: integer
+ */
+int wildcmp(char *one, char *two)
 {
+	if (*one == '\0')
+	{
+		if (*two != '\0' && *two != '*')
+			return (wildcmp(one, two + 1));
+		return (*two == '\0');
+	}
+	else if (*two == '*')
+		return (wildcmp(one + 1, two) || wildcmp(one, two + 1));
+	else if (*one == *two)
+		return (wildcmp(one + 1, two + 1))
+	return (0);
 }

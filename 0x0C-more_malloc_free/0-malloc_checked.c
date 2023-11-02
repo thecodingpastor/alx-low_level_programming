54c0e8a1c97e119badde13d3f0c99f6b8a11c0d2
Ayeni Michael <thecodingpastor@gmail.com>
2023-11-02 18:29:52 +0000

    add malloc-free part 2

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - a function that allocates memory using malloc
+ *
+ * @b: argument
+ * Return: a void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *var;
+
+	var = malloc(b);
+
+	if (var == NULL)
+	{
+		exit(98);
+	}
+	return (var);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat -  function that concatenates two strings.
+ *
+ * @s1: arg 1
+ * @s2: arg 2
+ * @n: arg 3
+ * Return: a pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0, firstLen = 0, secLen = 0;
+	char *var;
+
+	while (s1 && s1[firstLen])
+	{
+		firstLen++;
+	}
+	while (s2 && s2[secLen])
+		secLen++;
+
+	if (n < secLen)
+		var = malloc(sizeof(char) * (firstLen + n + 1));
+	else
+		var = malloc(sizeof(char) * (firstLen + secLen + 1));
+
+	if (!var)
+		return (NULL);
+	while (i < firstLen)
+	{
+		var[i] = s1[i];
+		i++;
+	}
+
+	while (n < secLen && i < (firstLen + n))
+	{
+		var[i] = s2[j];
+		i++, j++;
+	}
+	while (n >= secLen && i < (firstLen + secLen))
+	{
+		var[i] = s2[j];
+		i++, j++;
+	}
+	var[i] = '\0';
+
+	return (var);
+}

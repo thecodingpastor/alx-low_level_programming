6b2b7bb00b1619c402b3dac9bf80311d46a6cfb0
Ayeni Michael <thecodingpastor@gmail.com>
2023-11-16 20:54:20 +0000

    added singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list from memory
+ * @head: arg
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
